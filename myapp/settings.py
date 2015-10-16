7e834b1d82b9a6f0ff63a731498a11915bbb5381
kaye <kwattawa@fusionbox.com>
2015-10-16 13:06:50 -0600

    additional files and changes for Heroku

--- myapp/settings.py
+++ myapp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scheduler',
+        'USER': 'kaye',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
@@ -102,3 +106,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+	from .local_settings import *
+except ImportError:
+	pass
+

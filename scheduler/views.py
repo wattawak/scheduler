9387d2711f6156ff618723a2d56de7ba2b688411
kaye <kwattawa@fusionbox.com>
2015-10-16 15:17:43 -0600

    added homepage, base.html, first view

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def project_list(request):
+	projects = Project.objects.all()
+	return render(request, 'scheduler/project_list.html', {'projects': projects})

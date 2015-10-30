1b3f151837bf377f8e9bdb61e1630c6dd3d4d633
kaye <kwattawa@fusionbox.com>
2015-10-30 17:32:16 -0600

    refactor code to have separate project detail view

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.project_list, name='project_list'),
+	url(r'^$', views.ProjectList.as_view(), name='project_list'),
+	url(r'^project/(?P<pk>[0-9]+)/$', views.ProjectDetail.as_view(), name = 'project_detail'),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,21 +1,52 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.views.generic import ListView, DetailView, FormView, View
+from django.core.urlresolvers import reverse
+from django.views.generic.detail import SingleObjectMixin
+
+
 from .models import Project
 from .forms import RescheduleForm
 
+class ProjectList(ListView):
+	model = Project
+	template_name = 'scheduler/project_list.html'
+	context_object_name = 'project_list'
+	def get_queryset(self):
+		return Project.objects.order_by('start_date')
+		
+		
+class ProjectDetailView(DetailView):
+	model = Project
+	context_object_name = 'project_detail'
+	
+	def get_context_data(self, **kwargs):
+		context = super(ProjectDetailView, self).get_context_data(**kwargs)
+		context['form'] = RescheduleForm()
+		return context		
+	
+class ProjectDetailFormView(SingleObjectMixin, FormView):
+	template_name = 'scheduler/project_detail.html'
+	form_class = RescheduleForm
+	model = Project
+	
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object()
+		self.object.reschedule(days_later)
+		self.object.save()
+	
+	def get_success_url(self):
+		return reverse('project_detail', kwargs={'pk': self.object.pk})
+		
+	
+class ProjectDetail(View):
 
-def project_list(request):
-	if request.method == "POST":
-		form = RescheduleForm(request.POST)
-		if form.is_valid():
-			days_later = form.cleaned_data['days_later']
-			project_id = form.cleaned_data['project_id']
-			project = Project.objects.get(pk=project_id)
-			project.reschedule(days_later)
-			project.save()
-	else:
-		form = RescheduleForm()
-	projects = Project.objects.order_by('start_date')
-	return render(request, 'scheduler/project_list.html', {'projects': projects, 'form': form})
+	def get(self, request, *args, **kwargs):
+		view = ProjectDetailView.as_view()
+		return view(request, *args, **kwargs)
+		
+	def post(self, request, *args, **kwargs):
+		view = ProjectDetailFormView.as_view()
+		return view(request, *args, **kwargs)
 
 
